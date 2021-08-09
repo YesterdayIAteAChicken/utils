2d54e05f7a5852a7d6ada41da8efe7a2c1bd90b8
mowuying <1710583392@qq.com>
2021-08-09 11:09:27 +0800

    first commit

---
+++ utils/src/main/java/com/cmy/utils/Utils.java
@@ -0,0 +1,16 @@
+package com.cmy.utils;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * author  :   mowuying
+ * date    :   2021/8/9
+ */
+public class Utils {
+
+    public static void testToast(Context cxt){
+        Toast.makeText(cxt, "工具库吐司测试", Toast.LENGTH_SHORT).show();
+    }
+
+} 
